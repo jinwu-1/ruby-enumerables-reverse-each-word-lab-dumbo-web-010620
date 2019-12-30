88755dd232f79c83cb0803e661ecaca2e5cf6f14
Jin Wu <jinn.wuu@gmail.com>
2019-12-30 19:34:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,7 @@
 def reverse_each_word(array)
-  new_array = array.split(" ")
-  new_array.collect {|x| x.reverse!}
-  new_array.join(" ")
+new_array = array.split(" ")
+  new_array.collect do |x|
+    x.reverse!
+  end
+new_array.join(" ")
 end
